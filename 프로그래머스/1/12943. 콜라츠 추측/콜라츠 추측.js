62957bd4b90f084cee813d1fae2995d1dc458e27
visionSpoiler <wjdgns7565@gmail.com>
2025-08-07 21:18:26 +0900

    [level 1] Title: 콜라츠 추측, Time: 0.26 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/1/12943. 콜라츠 추측/콜라츠 추측.js
@@ -0,0 +1,16 @@
+function solution(num) {
+    let count = 0;
+    while(count <=500) {
+        if(num==1) {
+            return count;
+        }
+        if(num%2 == 0) {
+            num /= 2;
+        }
+        else if(num%2 == 1) {
+            num = num*3+1
+        }
+        count++;
+    }
+    return -1
+}
