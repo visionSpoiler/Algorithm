d9ae94d677047ca1a2781810e0a5ee7e565abc4c
visionSpoiler <wjdgns7565@gmail.com>
2025-08-23 17:02:57 +0900

    [level 1] Title: 문자열 내 마음대로 정렬하기, Time: 41.42 ms, Memory: 35.9 MB -BaekjoonHub

---
+++ 프로그래머스/1/12915. 문자열 내 마음대로 정렬하기/문자열 내 마음대로 정렬하기.js
@@ -0,0 +1,10 @@
+function solution(strings, n) {
+    return strings.sort((a,b) => {
+        if(a[n] === b[n]) {
+            return a.localeCompare(b)
+        }
+        else {
+            return a[n].localeCompare(b[n])
+        }
+    })
+}
